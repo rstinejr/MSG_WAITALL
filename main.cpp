d7e5cc994ea9d4c0f41b11f5f28891fa73860294
rstinejr <bob@waltonstine.net>
2016-07-27 20:05:13 -0400

    Add signal handler that calls exit(0)

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <errno.h>
 #include <inttypes.h>
 #include <pthread.h>
+#include <signal.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include<iostream>
 
@@ -13,7 +15,29 @@ void *server_routine(void *arg)
 	uint16_t port = *((uint16_t *) arg);
 	cout << "Listen on port " << port << endl;
 
+	pause();
+
 	return NULL;
+}
+
+void sig_handler(int s)
+{
+	cout << endl << "Caught signal " << s << ", so exit." << endl;
+	exit(0);
+}
+
+void setupHandler()
+{
+    struct sigaction sigAction;
+
+	sigAction.sa_handler = sig_handler;
+	sigAction.sa_flags = 0;
+
+	sigaction(SIGHUP,  &sigAction, NULL);
+	sigaction(SIGINT,  &sigAction, NULL);
+	sigaction(SIGQUIT, &sigAction, NULL);
+	sigaction(SIGKILL, &sigAction, NULL);
+	sigaction(SIGTERM, &sigAction, NULL);
 }
 
 int main(int argc, char *argv[])
@@ -23,6 +47,8 @@ int main(int argc, char *argv[])
 	uint16_t port = (argc > 1) ? atoi(argv[1]) : DEFAULT_PORT;
 
 	cout << "Start demo. Signal to exit." << endl;
+
+	setupHandler();
 
 	pthread_t server_tid;
 
